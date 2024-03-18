7c488802459a631ba3ce7b93d89e5255e2c7b7ab
Jeff Lovell <jeff@lovellworks.com>
2024-03-18 01:27:29 -0400

    setup... the grid... and cue tron music

--- sketch.js
+++ sketch.js
@@ -1,7 +1,44 @@
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+let grid;
+let cols;
+let rows;
+let resolution = 20;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  cols = width / resolution;
+  rows = height / resolution;
+
+  grid = make2DArray(cols,rows)
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = floor(random(2));
+    }
+  }
 }
 
 function draw() {
-  background(220);
+  background(0);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fill(255);
+        stroke(225);
+        rect(x, y, resolution - 1, resolution - 1);
+      } else {
+        fill(150, 0, 0);
+        stroke(225);
+        rect(x, y, resolution - 1, resolution - 1);
+      }
+    }
+  }
 }
